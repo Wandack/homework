4cd42d4a6b1a4d1626df2d963628937c2a791c27
Bohdan <wedmedikbo@gmail.com>
2019-08-21 09:58:38 +0300

    Second

--- src/homework.java
+++ src/homework.java
@@ -1,6 +1,5 @@
 public class homework {
     public static void main(String[] args) {
         System.out.println("My Homework, really!");
-        System.out.println("Ничего не понимаю, не Пушит и всё");
     }
 }
