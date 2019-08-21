44c37c29a190e50a3b47f3e11e47849d2bb17cc5
Bohdan <wedmedikbo@gmail.com>
2019-08-21 09:57:13 +0300

    First ++

--- src/homework.java
+++ src/homework.java
@@ -1,5 +1,6 @@
 public class homework {
     public static void main(String[] args) {
         System.out.println("My Homework, really!");
+        System.out.println("Ничего не понимаю, не Пушит и всё");
     }
 }
